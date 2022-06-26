3959cd5d15750f068129a7a23a18d02b2ef64b33
Lacia~chan <52329982+Lyceris-chan@users.noreply.github.com>
2022-06-26 13:10:06 +0200

    Upload initial module

---
+++ service.sh
@@ -0,0 +1,17 @@
+#!/system/bin/sh
+while [ "$(getprop sys.boot_completed | tr -d '\r')" != "1" ]; do sleep 1; done
+
+# https://issuetracker.google.com/issues/80270303?pli=1
+cp /data/adb/modules/ReVanced/com.google.android.youtube_17.23.36.apk /data/local/tmp
+pm install -d /data/local/tmp/com.google.android.youtube_17.23.36.apk
+rm -rf /data/local/tmp/com.google.android.youtube_17.23.36.apk
+rm -rf /data/adb/modules/ReVanced/com.google.android.youtube_17.23.36.apk
+
+base_path="/data/adb/modules/ReVanced/com.google.android.youtube.apk"
+stock_path=$( pm path com.google.android.youtube | grep base | sed 's/package://g' )
+
+chcon u:object_r:apk_data_file:s0  $base_path
+mount -o bind $base_path $stock_path
+
+sed '/tmp/d' /data/adb/modules/ReVanced/service.sh > changed.txt && mv changed.txt /data/adb/modules/ReVanced/service.sh
+sed '/https/d' /data/adb/modules/ReVanced/service.sh > changed.txt && mv changed.txt /data/adb/modules/ReVanced/service.sh
