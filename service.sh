526a19436ce3fc3521cc4fbfb42d61da716ac508
Lyceris-chan <ghostdrain@outlook.com>
2022-06-26 11:23:43 +0000

    Update for 17.24.34

--- service.sh
+++ service.sh
@@ -2,12 +2,12 @@
 while [ "$(getprop sys.boot_completed | tr -d '\r')" != "1" ]; do sleep 1; done
 
 # https://issuetracker.google.com/issues/80270303?pli=1
-cp /data/adb/modules/ReVanced/com.google.android.youtube_17.23.36.apk /data/local/tmp
-pm install -d /data/local/tmp/com.google.android.youtube_17.23.36.apk
-rm -rf /data/local/tmp/com.google.android.youtube_17.23.36.apk
-rm -rf /data/adb/modules/ReVanced/com.google.android.youtube_17.23.36.apk
+cp /data/adb/modules/ReVanced/com.google.android.youtube.apk /data/local/tmp
+pm install -d /data/local/tmp/com.google.android.youtube.apk
+rm -rf /data/local/tmp/com.google.android.youtube.apk
+rm -rf /data/adb/modules/ReVanced/com.google.android.youtube.apk
 
-base_path="/data/adb/modules/ReVanced/com.google.android.youtube.apk"
+base_path="/data/adb/modules/ReVanced/revanced-root-signed.apk"
 stock_path=$( pm path com.google.android.youtube | grep base | sed 's/package://g' )
 
 chcon u:object_r:apk_data_file:s0  $base_path
