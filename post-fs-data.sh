5b9f267189dc86cb413b0477e455a872f4ccef3c
shimpact003 <42101670+shimpact003@users.noreply.github.com>
2022-07-27 00:51:33 +0300

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,4 +1,6 @@
 #!/system/bin/sh
 
-stock_path=$( pm path com.google.android.youtube | grep base | sed 's/package://g' )
-umount -l $stock_path
+stock_youtube_path=$( pm path com.google.android.youtube | grep base | sed 's/package://g' )
+stock_music_path=$( pm path com.google.android.apps.youtube.music | grep base | sed 's/package://g' )
+umount -l $stock_youtube_path
+umount -l $stock_music_path
